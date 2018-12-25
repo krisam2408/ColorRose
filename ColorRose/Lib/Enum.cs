676bfc5e8e32cc726c896ef0bd242c3f8fc75668
krisam2408 <cbornhardt@outlook.com>
2018-12-25 18:12:03 -0300

    Create Enum.cs

---
+++ ColorRose/Lib/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorRose.Lib
+{
+    public enum HarmoniesType
+    {
+        Monochromatic, Analogous, Triadric, Complimentary, SplitComplimentary, DoubleComplimentary
+    }
+}
