0c57083fd31e2e630db9950b80b1cc27c7008cce
Christoph Bornhardt <cbornhardt@outlook.com>
2021-06-02 00:03:33 -0400

    Create ColorControl.cs

---
+++ ColorRoseWPF/Core/ColorControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ColorRoseWPF.Core
+{
+    public class ColorControl
+    {
+        public string Name { get; set; }
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+
+        private int _value;
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    propertyChangedAction.Invoke();
+                }
+            }
+        }
+
+        private Action propertyChangedAction;
+
+        public ColorControl(Action propertyChangedAction)
+        {
+            this.propertyChangedAction = propertyChangedAction;
+        }
+    }
+}
